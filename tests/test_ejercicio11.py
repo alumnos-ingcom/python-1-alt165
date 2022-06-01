0f3819889e64c9ac9a24ea8d345a9025b7a7ebb7
=Jose Lambrechts <alt165@gmail.com>
2022-06-01 00:21:01 -0300

    agregar docstrings y modificar comparadores

--- tests/test_ejercicio11.py
+++ tests/test_ejercicio11.py
@@ -1,3 +1,5 @@
+"""prueba de la funcion es_multiplo del ejercicio 11
+"""
 from src.ejercicio11 import es_multiplo
 
 def test_mayor_menor_no_multiplo():
@@ -6,7 +8,7 @@ def test_mayor_menor_no_multiplo():
     posible_multiplo = 15
     resultado = es_multiplo(numero, posible_multiplo)
     assert isinstance(resultado, bool), "El resultado debe ser un valor lógico"
-    assert resultado == False, "No obtenemos un resultado esperado"
+    assert resultado is False, "No obtenemos un resultado esperado"
 
 def test_mayor_menor_multiplo():
     """prueba primer numero mayor, segundo menor, si es multiplo"""
@@ -14,14 +16,14 @@ def test_mayor_menor_multiplo():
     posible_multiplo = 20
     resultado = es_multiplo(numero, posible_multiplo)
     assert isinstance(resultado, bool), "El resultado debe ser un valor lógico"
-    assert resultado == True, "No obtenemos un resultado esperado"
+    assert resultado is True, "No obtenemos un resultado esperado"
 
 def test_iguales_multiplo():
     """prueba numeros iguales"""
     numero = 10
     resultado = es_multiplo(numero, numero)
     assert isinstance(resultado, bool), "El resultado debe ser un valor lógico"
-    assert resultado == True, "No obtenemos un resultado esperado"
+    assert resultado is True, "No obtenemos un resultado esperado"
 
 if __name__ == "__main__":
     sys.exit(pytest.main())
